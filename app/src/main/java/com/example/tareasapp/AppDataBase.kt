4d33a9fcfd6294ed794581e2cfd7a9eb28eecc23
Barca6ta2024 <migueldegollado20@gmail.com>
2026-05-19 14:35:00 -0700

    bump db version to 2 and add fallbackToDestructiveMigration

--- app/src/main/java/com/example/tareasapp/AppDataBase.kt
+++ app/src/main/java/com/example/tareasapp/AppDataBase.kt
@@ -8,11 +8,10 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [TaskEntity::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
-
-abstract class AppDataBase: RoomDatabase() {
+abstract class AppDataBase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
 
     companion object {
@@ -20,21 +19,18 @@ abstract class AppDataBase: RoomDatabase() {
         @Volatile
         private var INSTANCE: AppDataBase? = null
 
-        fun getInstance(
-            context: Context,
-        ): AppDataBase {
+        fun getInstance(context: Context): AppDataBase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDataBase::class.java,
                     "tasks_db"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
                 INSTANCE = instance
                 instance
-
             }
-
         }
     }
 }
-
